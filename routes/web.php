04f10e0e3b514be4e58bd4b566b7037d9203a81b
liuchaoqiang <17600084468@163.com>
2020-09-11 16:25:30 +0800

    用户个人原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
