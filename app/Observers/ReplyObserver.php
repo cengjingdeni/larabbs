63474df7fc555c42b05ab512ee024625bc38bedc
liuchaoqiang <17600084468@163.com>
2020-12-14 18:05:33 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -12,10 +12,18 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
     {
         //
+    }
+
+    public function created(Reply $reply)
+    {
+        $topic= $reply->topic()->first(['id','reply_count']);
+        $topic->reply_count =$topic->replies()->count();
+        $topic->save();
     }
 }
