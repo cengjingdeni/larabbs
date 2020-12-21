6efa248584e21cee9651704e3905aca76bac421b
liuchaoqiang <17600084468@163.com>
2020-12-21 17:55:24 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       <a href="#" target="_blank">chaoqiang_liu</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:17600084468@163.com">call me</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">call me</a></p>
   </div>
 </footer>
